5e75c656765c33e0b565aa534f9d07516b93dbeb
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-06-27 14:16:40 -0400

    checkFields function now checks for mandatory fields, and sends alert if user has not filled them in. If they have, it stores the inputs in thecorrect global variable

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,18 +1,36 @@
+
+//Gobal variables for calculating bill. 
 var billTotal = 0;
 var tipPercentage = 0;
+// These two values are optional, since the user may not want to round the tip or split between anyone.
 var splitBetween = 0;
-var Rounding = 0;
+var rounding = 0;
 
 $("#submit-btn").on("click", function() {
+    // prevent button from trying to submit the form to a server.
     event.preventDefault();
     checkFields();
 })
 
 function checkFields() {
     if ($("#bill-total").val() === "") {
-        alert("You haven't entered a bill total.")
-    } else {
+        alert("You haven't entered a bill total.");
+    }
+     else if ($('input:radio[name=tip-percentage]:checked').val() == undefined){
+        alert("You must select a percentage you would like to tip.");
+    }
+     else {
+         if($('input:radio[name=rounding]:checked').val() == undefined) {
+
+         } else {
+             rounding = $('input:radio[name=rounding]:checked').val();
+         }
     billTotal = $("#bill-total").val().trim();
-    console.log(billTotal);
+    tipPercentage = $('input:radio[name=tip-percentage]:checked').val()
+    console.log(billTotal, tipPercentage, rounding);
     }   
+}
+
+function calculateTip() {
+
 }
