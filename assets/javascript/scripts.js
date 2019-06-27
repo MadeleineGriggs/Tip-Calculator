77a9859891ef6c02adefa650d087a56ea797be01
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-06-27 14:33:05 -0400

    added functions so the bill total field only accepts numbers and decimals. added function so the custom percentage and split between fields accept only numbers.

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -12,6 +12,7 @@ $("#submit-btn").on("click", function() {
     checkFields();
 })
 
+
 function checkFields() {
     if ($("#bill-total").val() === "") {
         alert("You haven't entered a bill total.");
@@ -34,3 +35,25 @@ function checkFields() {
 function calculateTip() {
 
 }
+
+// Function allows only numbers, backspace, delete, and one decimal to be added to the 
+// fields with the class "numbers-decimals-only"
+$(".numbers-decimals-only").keypress(function (e) {
+    if(e.which == 46){
+        if($(this).val().indexOf('.') != -1) {
+            return false;
+        }
+    }
+
+    if (e.which != 8 && e.which != 0 && e.which != 46 && (e.which < 48 || e.which > 57)) {
+        return false;
+    }
+});
+
+// Function allows only numbers to be added to the fields with the class "numbers-only".
+$(".numbers-only").keypress(function( event ){
+    var key = event.which;
+
+    if( ! ( key >= 48 && key <= 57 ) )
+        event.preventDefault();
+});
